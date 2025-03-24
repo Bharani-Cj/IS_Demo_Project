9a697bc685e291b3b93bc6eb94e293b5672bd01b
Ber-08 <bezytryphena.com>
2025-03-24 10:44:01 +0530

    240325

--- index.js
+++ index.js
@@ -1,91 +1,91 @@
-
-
 let products = [
   {
-    name: 'Fastrack Radiant FX4 Premium Metal',
+    name: "Fastrack Radiant FX4 Premium Metal",
     price: 5495,
-    imgUrl: 'img1.jpg',
-    type: 'men',
+    imgUrl: "img1.jpg",
+    type: "men",
     id: 1,
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin velnulla id orci gravida ullamcorper.",
-    quantity: 0
+    quantity: 0,
   },
   {
-    name: 'Fastrack Radiant FX1 Luxury Metal',
+    name: "Fastrack Radiant FX1 Luxury Metal",
     price: 4999,
-    imgUrl: 'img2.jpg',
-    type: 'men',
+    imgUrl: "img2.jpg",
+    type: "men",
     id: 2,
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin velnulla id orci gravida ullamcorper.",
-    quantity: 0
+    quantity: 0,
   },
   {
-    name: 'Fastrack Vyb Challenger Quartz Analog',
+    name: "Fastrack Vyb Challenger Quartz Analog",
     price: 1695,
-    imgUrl: 'img3.jpg',
-    type: 'men',
+    imgUrl: "img3.jpg",
+    type: "men",
     id: 3,
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin velnulla id orci gravida ullamcorper.",
-    quantity: 0
+    quantity: 0,
   },
   {
-    name: 'Fastrack Radiant FX4 Premium Metal',
+    name: "Fastrack Radiant FX4 Premium Metal",
     price: 1200,
-    imgUrl: 'img4.jpg',
-    type: 'men',
+    imgUrl: "img4.jpg",
+    type: "men",
     id: 4,
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin velnulla id orci gravida ullamcorper.",
-    quantity: 0
+    quantity: 0,
   },
   {
-    name: 'Fastrack Rogue with 3.5 CM UltraVU',
+    name: "Fastrack Rogue with 3.5 CM UltraVU",
     price: 6000,
-    imgUrl: 'img5.jpg',
-    type: 'women',
+    imgUrl: "img5.jpg",
+    type: "women",
     id: 5,
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin velnulla id orci gravida ullamcorper.",
-    quantity: 0
+    quantity: 0,
   },
   {
-    name: 'Fastrack Vyb Challenger Quartz Analog',
+    name: "Fastrack Vyb Challenger Quartz Analog",
     price: 4000,
-    imgUrl: 'img6.jpg',
-    type: 'women',
+    imgUrl: "img6.jpg",
+    type: "women",
     id: 6,
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin velnulla id orci gravida ullamcorper.",
-    quantity: 0
+    quantity: 0,
   },
   {
-    name: 'Fastrack I Love Me Quartz Analog',
+    name: "Fastrack I Love Me Quartz Analog",
     price: 4500,
-    imgUrl: 'img7.jpg',
-    type: 'women',
+    imgUrl: "img7.jpg",
+    type: "women",
     id: 7,
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin velnulla id orci gravida ullamcorper.",
-    quantity: 0
+    quantity: 0,
   },
   {
-    name: 'Fastrack Quartz Analog Silver Dial',
+    name: "Fastrack Quartz Analog Silver Dial",
     price: 3200,
-    imgUrl: 'img8.jpg',
-    type: 'women',
+    imgUrl: "img8.jpg",
+    type: "women",
     id: 8,
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin velnulla id orci gravida ullamcorper.",
-    quantity: 0
-  }
-]
+    quantity: 0,
+  },
+];
 
 function goToProduct(id) {
-  let product = products.find(product => product.id === id); 
+  let product = products.find((product) => product.id === id);
   window.location.href = "product.html?id=" + id;
 }
 
 function loadProduct() {
   let urlParams = new URLSearchParams(window.location.search);
-  let id = urlParams.get('id');
-  let product = id ? products.find(product => product.id === +(id)) : products[0];
-  let container = document.getElementsByClassName('product-container')[0];
-  container.innerHTML = '';
+  let id = urlParams.get("id");
+  let product = id
+    ? products.find((product) => product.id === +id)
+    : products[0];
+  let container = document.getElementsByClassName("product-container")[0];
+  container.innerHTML = "";
   container.innerHTML = `
     <div class="col-md-6 text-center">
       <img
@@ -100,7 +100,7 @@ function loadProduct() {
       <p>${product.desc}</p>
       <button class="btn btn-success" onclick="addToCart(${product.id})">Add to Cart</button>
     </div>
-  `
+  `;
   //view item
   if (window.Evergage) {
     Evergage.sendEvent({
@@ -111,38 +111,38 @@ function loadProduct() {
           name: product.name,
           price: product.price,
           imageUrl: `../imgs/${product.imgUrl}`,
-          description: product.desc
-        }
-      }
+          description: product.desc,
+        },
+      },
     });
   }
 }
 
 function addToCart(id) {
   let addedItems = [];
-  let product = products.find(product => product.id === +(id));
-  addedItems = JSON.parse(localStorage.getItem('products'));
+  let product = products.find((product) => product.id === +id);
+  addedItems = JSON.parse(localStorage.getItem("products"));
   if (addedItems && addedItems.length > 0) {
-    let index = addedItems.findIndex(product => product.id === +(id));
+    let index = addedItems.findIndex((product) => product.id === +id);
     if (index != -1) {
       addedItems[index].quantity += 1;
-      localStorage.setItem('products', JSON.stringify(addedItems));      
+      localStorage.setItem("products", JSON.stringify(addedItems));
     } else {
-      addedItems.push({...product, quantity: 1});
+      addedItems.push({ ...product, quantity: 1 });
     }
   } else {
-    addedItems = [{...product, quantity: 1}];
+    addedItems = [{ ...product, quantity: 1 }];
   }
-  localStorage.setItem('products', JSON.stringify(addedItems));
+  localStorage.setItem("products", JSON.stringify(addedItems));
   loadCart();
 
-  // Evergage Add to Cart Tracking
+  //! Evergage Add to Cart Tracking
   if (window.Evergage) {
     const lineItem = {
       _id: product.id.toString(),
       name: product.name,
       price: product.price,
-      quantity: 1, 
+      quantity: 1,
     };
 
     Evergage.sendEvent({
@@ -154,23 +154,23 @@ function addToCart(id) {
       },
     });
     console.log("Evergage Add to Cart event fired:", lineItem);
-    
   }
 }
-
 
 function loadCart() {
   document.getElementById("cart-count");
   let products = JSON.parse(localStorage.getItem("products"));
   if (products) {
-    document.getElementById("cart-count").textContent = products.reduce((sum, item) => sum + item.quantity, 0);
+    document.getElementById("cart-count").textContent = products.reduce(
+      (sum, item) => sum + item.quantity,
+      0
+    );
   } else {
     document.getElementById("cart-count").textContent = 0;
   }
 }
 
 function goToCart() {
-
   window.location.href = "cart.html";
 }
 
@@ -191,32 +191,41 @@ function loadCartItems() {
       <td>${item.price}</td>
       <td class="action-icons">
           <div class="quantity-controls">
-              <button type="button" class="quantity-btn" onclick="changeQuantity(${item.id}, 'minus')">-</button>
+              <button type="button" class="quantity-btn" onclick="changeQuantity(${
+                item.id
+              }, 'minus')">-</button>
               <span>${item.quantity}</span>
-              <button  type="button" class="quantity-btn" onclick="changeQuantity(${item.id}, 'add')">+</button>
-              <button class="remove-btn" onclick="removeFromCart(${item.id})">Remove</button>
+              <button  type="button" class="quantity-btn" onclick="changeQuantity(${
+                item.id
+              }, 'add')">+</button>
+              <button class="remove-btn" onclick="removeFromCart(${
+                item.id
+              })">Remove</button>
           </div>
       </td>
-    `
+    `;
     cartContainer.appendChild(cartItem);
-    total += (item.price * item.quantity);
+    total += item.price * item.quantity;
   });
 
   cartTotal.innerText = total.toFixed(2);
-  let elem = document.getElementById('btn-purchase-icon');
+  let elem = document.getElementById("btn-purchase-icon");
   if (products && products.length > 0) {
-    elem.getElementsByClassName('btn-purchase')[0].style.display = 'block';
+    elem.getElementsByClassName("btn-purchase")[0].style.display = "block";
   } else {
-    elem.getElementsByClassName('btn-purchase')[0].style.display = 'none';
+    elem.getElementsByClassName("btn-purchase")[0].style.display = "none";
   }
 }
 
 function changeQuantity(id, method) {
   // console.log(item)
   let products = JSON.parse(localStorage.getItem("products"));
-  let index = products.findIndex(product => product.id === id);
+  let index = products.findIndex((product) => product.id === id);
   if (index !== -1) {
-    products[index].quantity = method == 'add' ? products[index].quantity + 1 : products[index].quantity - 1;
+    products[index].quantity =
+      method == "add"
+        ? products[index].quantity + 1
+        : products[index].quantity - 1;
     if (products[index].quantity <= 0) {
       products.splice(index, 1);
     }
@@ -227,48 +236,47 @@ function changeQuantity(id, method) {
 
 function removeFromCart(id) {
   let products = JSON.parse(localStorage.getItem("products"));
-  let index = products.findIndex(product => product.id === id);
+  let index = products.findIndex((product) => product.id === id);
 
   if (index != -1) {
     const removedProduct = products[index];
     products.splice(index, 1);
     localStorage.setItem("products", JSON.stringify(products));
-    loadCartItems(); 
-  
-  // Evergage Remove from Cart Event
-  if (window.Evergage) {
-    const lineItem = {
-      _id: removedProduct.id.toString(),
-      name: removedProduct.name,
-      price: removedProduct.price,
-      imageUrl: `../imgs/${removedProduct.imgUrl}`,
-      quantity: removedProduct.quantity, 
-    };
+    loadCartItems();
 
-    Evergage.sendEvent({
-      itemAction: Evergage.ItemAction.RemoveFromCart,
-      cart: {
-        singleLine: {
-          Product: lineItem,
+    //! Evergage Remove from Cart Event
+    if (window.Evergage) {
+      const lineItem = {
+        _id: removedProduct.id.toString(),
+        name: removedProduct.name,
+        price: removedProduct.price,
+        imageUrl: `../imgs/${removedProduct.imgUrl}`,
+        quantity: removedProduct.quantity,
+      };
+
+      Evergage.sendEvent({
+        itemAction: Evergage.ItemAction.RemoveFromCart,
+        cart: {
+          singleLine: {
+            Product: lineItem,
+          },
         },
-      },
-    });         
-    
+      });
+    }
   }
-}
 }
 
 function loadProducts(type) {
   let filteredProducts = [];
-  if (type === 'all') {
+  if (type === "all") {
     filteredProducts = JSON.parse(JSON.stringify(products));
   } else {
-    filteredProducts = products.filter(product => product.type === type);
+    filteredProducts = products.filter((product) => product.type === type);
   }
-  let container = document.getElementsByClassName('product-container')[0];
-  container.innerHTML = '';
-  filteredProducts.map(product => {
-    let column = createElement('div', ['col', 'filter-item', 'all', 'new']);
+  let container = document.getElementsByClassName("product-container")[0];
+  container.innerHTML = "";
+  filteredProducts.map((product) => {
+    let column = createElement("div", ["col", "filter-item", "all", "new"]);
     column.innerHTML = `
       <div class="card h-100 product" onclick="goToProduct(${product.id})">
         <img src="../imgs/${product.imgUrl}" class="card-img-top shop-item-image" alt="...">
@@ -280,29 +288,29 @@ function loadProducts(type) {
       </div>
     `;
     container.appendChild(column);
-  })
-  
+  });
+
   if (window.Evergage) {
     Evergage.sendEvent({
       itemAction: Evergage.ItemAction.ViewCategory,
       catalog: {
         Category: {
-          id: type, 
-          name: type.charAt(0).toUpperCase() + type.slice(1), 
-          productCount: filteredProducts.length, 
+          id: type,
+          name: type.charAt(0).toUpperCase() + type.slice(1),
+          productCount: filteredProducts.length,
         },
       },
     });
-    console.log("Evergage View Category event fired:", type); 
+    console.log("Evergage View Category event fired:", type);
   }
 }
 
 function createElement(type, classNames) {
   let elem = document.createElement(type);
-  classNames.forEach(name => {
+  classNames.forEach((name) => {
     elem.classList.add(name);
-  })
-  return elem
+  });
+  return elem;
 }
 
 function onPurchase() {
@@ -313,54 +321,59 @@ function onPurchase() {
     return;
   }
 
-  let totalValue = products.reduce((sum, item) => sum + item.price * item.quantity, 0);
+  let totalValue = products.reduce(
+    (sum, item) => sum + item.price * item.quantity,
+    0
+  );
 
   let orderId = `ORDER_${Date.now()}`;
 
-  let lineItems = products.map(item => ({
-    _id: item.id.toString(), 
-    name: item.name, 
-    price: item.price, 
-    imageUrl: `../imgs/${item.imgUrl}`, 
-    quantity: item.quantity, 
+  let lineItems = products.map((item) => ({
+    _id: item.id.toString(),
+    name: item.name,
+    price: item.price,
+    imageUrl: `../imgs/${item.imgUrl}`,
+    quantity: item.quantity,
   }));
 
-  // Evergage Purchase Event
+  //! Evergage Purchase Event
   if (window.Evergage) {
     Evergage.sendEvent({
-      itemAction: 'Purchase',
+      itemAction: "Purchase",
       order: {
         Product: {
           orderId: orderId,
-          totalValue: totalValue, 
-          currency: 'INR', 
-          lineItems: lineItems, 
+          totalValue: totalValue,
+          currency: "INR",
+          lineItems: lineItems,
         },
       },
     });
-    console.log("Evergage Purchase event fired:", { orderId, totalValue, lineItems }); 
+    console.log("Evergage Purchase event fired:", {
+      orderId,
+      totalValue,
+      lineItems,
+    });
 
+    let elem = document.getElementById("btn-purchase-icon");
+    if (elem) {
+      elem.innerHTML = "Thank you for your purchase";
+    } else {
+      console.error("Element with ID 'btn-purchase-icon' not found.");
+    }
 
-  let elem = document.getElementById('btn-purchase-icon');
-  if (elem) {
-    elem.innerHTML = 'Thank you for your purchase';
-  } else {
-    console.error("Element with ID 'btn-purchase-icon' not found.");
+    if (typeof Storage !== "undefined") {
+      localStorage.setItem("products", JSON.stringify([]));
+    } else {
+      console.error("LocalStorage is not supported in this browser.");
+    }
+
+    if (typeof loadCartItems === "function") {
+      loadCartItems();
+    } else {
+      console.error("loadCartItems function is not defined.");
+    }
   }
-
-  
-  if (typeof Storage !== "undefined") {
-    localStorage.setItem('products', JSON.stringify([]));
-  } else {
-    console.error("LocalStorage is not supported in this browser.");
-  }
-
-  if (typeof loadCartItems === "function") {
-    loadCartItems();
-  } else {
-    console.error("loadCartItems function is not defined.");
-  }
-}
 }
 
 function ready() {
@@ -401,7 +414,6 @@ function removeCartItem(event) {
   var buttonClicked = event.target;
   buttonClicked.parentElement.parentElement.remove();
   updateCartTotal();
-
 }
 function quantityChanged(event) {
   var input = event.target;
@@ -419,14 +431,10 @@ function addToCartClicked(event) {
   var imageSrc = shopItem.getElementsByClassName("shop-item-image")[0].src;
   addItemToCart(title, price, imageSrc);
   updateCartTotal();
-
-
-
-  }
+}
 
 function addItemToCart(title, price, imageSrc) {
-console.log("item added to cart");
-
+  console.log("item added to cart");
 
   var cartRow = document.createElement("tr");
   cartRow.classList.add("cart-row");
@@ -462,7 +470,6 @@ console.log("item added to cart");
   cartRow
     .getElementsByClassName("cart-quantity-input")[0]
     .addEventListener("change", quantityChanged);
- 
 }
 
 function updateCartTotal() {
